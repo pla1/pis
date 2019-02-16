0050cf163817ab35d20167551ef216a3ff997f22
Patrick L Archibald <patrick.archibald@gmail.com>
2019-02-15 21:22:41 -0500

    Update readme

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 #
 # Build ping image servlet war file
 #
+# You will need to change the project directory on line 9
+# You will need to change the location of your servlet-api.jar on line 10.
+# You will need to change the Tomcat webapps directory on line 12.
+#
 cd /home/htplainf/projects/pis
-javac -cp .:/home/htplainf/projects/apache-tomcat-8.5.35/lib/servlet-api.jar -d WEB-INF/classes src/net/pla1/pis/PingImageServlet.java 
+javac -cp .:/home/htplainf/projects/apache-tomcat-8.5.35/lib/servlet-api.jar -d WEB-INF/classes src/net/pla1/pis/PingImageServlet.java
 jar -cvf pis.war *
 cp pis.war ~/projects/apache-tomcat-8.5.35/webapps/.
-xdg-open pis.war
